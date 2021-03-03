f1f446f38ecee00ae5092d3a4a8996a5bf3923ea
Jahidul islam <kzis1987@gmail.com>
2021-03-03 14:36:56 +0600

    Es6 and Javascript Practice

---
+++ index.js
@@ -0,0 +1,14 @@
+
+// console.log("hi")
+// let doSomething = () =>  'hi';
+
+// let arr = [1,2,5,8,9]
+// let doSomething = arr =>  arr;
+// console.log(doSomething(arr))
+
+
+// let arr = [1,2,5,8,9]
+// arr.map( (iteam)=> {
+
+//   console.log(iteam)
+// })
